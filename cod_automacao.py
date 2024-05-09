93ad54f1fada69b30a2492117ce13b84e9a28a39
Júlia Diniz <juliadinizvaladares@hotmail.com>
2024-05-09 16:13:35 -0300

    commit

--- cod_automacao.py
+++ cod_automacao.py
@@ -2,9 +2,10 @@
 # 1. abrir o sistema da empresa 
     # https://dlp.hashtagtreinamentos.com/python/intensivao/login
     
-# para instalar: pip install pyautogui
+# pip install pyautogui
+# pip install pandas numpy openpyxl
 import pyautogui # importando a biblioteca
-pyautogui.PAUSE = 1 # tempo entre os comandos
+pyautogui.PAUSE = 1.2 # tempo entre os comandos
 
 # pyautogui.click -> clicar com o mouse 
 # pyautogui.write -> escrever um texto
@@ -23,11 +24,32 @@ pyautogui.write("juliadiniz@hotmail.com")
 pyautogui.press("tab")
 pyautogui.write("123456")
 pyautogui.press("enter")
-# 2. cadastrar produtos 
-pyautogui.press("tab")
-
-# 3. Abrir/Importar a base de dados de produtos para cadastrar
-# pip install pandas numpy openpyxl
-
-
-
+# 2. Cadastrar produtos
+for linha in tabela.index:
+    # clicar no campo de código
+    pyautogui.click(x=653, y=294)
+    # pegar da tabela o valor do campo que a gente quer preencher
+    codigo = tabela.loc[linha, "codigo"]
+    # preencher o campo
+    pyautogui.write(str(codigo))
+    # passar para o proximo campo
+    pyautogui.press("tab")
+    # preencher o campo
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pd.isna(obs):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter") # cadastra o produto (botao enviar)
+    # dar scroll de tudo pra cima
+    pyautogui.scroll(5000)
+    # Passo 3: Repetir o processo de cadastro até o fim
--- cod_auxiliar.py
+++ cod_auxiliar.py
@@ -1,4 +1,4 @@
 import pyautogui
 import time 
-time.sleep(7)
+time.sleep(4)
 print(pyautogui.position()) # procurar a posição do mouse em determinado tempo de tela 
